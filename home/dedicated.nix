bc5c4e14670dc8afd0c1d46731c845b1d5e22622
pwnwriter <hey@pwnwriter.xyz>
2024-01-20 09:09:22 +0545

    chore(configs): move wofi to hypr-raw branch

--- home/dedicated.nix
+++ home/dedicated.nix
@@ -4,7 +4,6 @@
   config.home.file = {
     ".config/alacritty" = {source = ./configs/alacritty;};
     ".config/cava" = {source = ./configs/cava;};
-    ".config/wofi" = {source = ./configs/wofi;};
     ".config/bat" = {source = ./configs/bat;};
     ".config/tmux" = {source = ./configs/tmux;};
   };
