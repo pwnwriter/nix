ed3cd234a24d5d66a28f20e171d897f7592a091c
pwnwriter <hey@pwnwriter.xyz>
2024-02-18 10:55:49 +0545

    chore(gpg): set new gpg id for password-store

--- home/apps.nix
+++ home/apps.nix
@@ -40,7 +40,7 @@
   programs.password-store = {
     enable = true;
     settings = {
-      PASSWORD_STORE_KEY = "0xC14F852608E47657";
+      PASSWORD_STORE_KEY = "B681763F9B5BDF279A139E0C0544A89BC51920AA";
       PASSWORD_STORE_CLIP_TIME = "30";
     };
   };
