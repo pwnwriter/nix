3512a6b17ee9b0927bbca97e2e8bd5bc357754e4
pwnwriter <hey@pwnwriter.xyz>
2024-02-23 16:06:48 +0545

    chore(flake): removed unused bindings

--- darwin/pkgs.nix
+++ darwin/pkgs.nix
@@ -1,25 +1,17 @@
 { pkgs, ... }: {
   environment.systemPackages = with pkgs; [
-    # Development Tools
     gcc
     rustup
     go
 
-    # Editors
     neovim
-
-    # Terminal Emulators
     wezterm
 
-    # File and Text Search
     ripgrep
     fzf
 
-    # Version Control and Encryption
-    gnupg
     pass
 
-    # File Navigation
     tree
     just
     bat
@@ -28,11 +20,9 @@
     yazi
     presenterm
 
-    # System Monitoring
     htop
     neofetch
 
-    # Utilities
     stylua
     nixpkgs-fmt
     nil
--- flake.nix
+++ flake.nix
@@ -16,7 +16,7 @@
 
   };
 
-  outputs = inputs@{ self, nixpkgs, darwin, home-manager, ... }: {
+  outputs = { self, nixpkgs, darwin, home-manager, ... }: {
     darwinConfigurations.pwnwriter = darwin.lib.darwinSystem {
       system = "aarch64-darwin";
       modules = [
