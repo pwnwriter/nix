8ade01389b2fbc48627be461eb15b940466b4d67
pwnwriter <hey@pwnwriter.xyz>
2024-06-20 22:36:12 +0545

    feat(direnv): use direnv for dev-shells; made mods more redable

--- modules/default.nix
+++ modules/default.nix
@@ -1,20 +1,23 @@
 { config, ... }:
 
+let
+  modules = [
+    ./bat.nix
+    ./git.nix
+    ./gpg.nix
+    ./ssh.nix
+    ./zsh.nix
+    ./pass.nix
+    ./pkgs.nix
+    ./yazi.nix
+    ./zoxide.nix
+    ./starship.nix
+    ./dedicated.nix
+    ./direnv.nix
+  ];
+in
 {
-  imports =
-    [
-      ./bat.nix
-      ./git.nix
-      ./gpg.nix
-      ./ssh.nix
-      ./zsh.nix
-      ./pass.nix
-      ./pkgs.nix
-      ./yazi.nix
-      ./zoxide.nix
-      ./starship.nix
-      ./dedicated.nix
-    ];
+  imports = modules;
 
   xdg.dataHome = "${config.home.homeDirectory}/.local/share";
   programs.home-manager.enable = true;
