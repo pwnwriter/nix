48b5b610d944ad195772b048d1eb4ee806c68d3c
pwnwriter <hey@pwnwriter.xyz>
2024-06-26 19:37:39 +0545

    feat(yazi); download mocha theme from catppuccin

--- modules/yazi.nix
+++ modules/yazi.nix
@@ -1,8 +1,16 @@
+{ pkgs, ... }:
+
+let
+  src = pkgs.fetchurl {
+    url = "https://raw.githubusercontent.com/catppuccin/yazi/main/themes/mocha.toml";
+    sha256 = "1s66068fjyd03hfzg8sb24lxk0n3ix5mgsaynw59rqcwyj3ix3p3";
+  };
+in
 {
   programs.yazi = {
     enable = true;
     enableZshIntegration = true;
   };
 
-}       
-        
+home.file.".config/yazi/theme.toml".source = src;
+}
