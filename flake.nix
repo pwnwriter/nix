b4ad3aecae8678e3d9757091a80d48510a6443b9
pwnwriter <hey@pwnwriter.xyz>
2024-07-02 16:12:10 +0545

    feat(flake): manage server inputs using flakes

--- flake.nix
+++ flake.nix
@@ -7,36 +7,54 @@
       url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
     };
+    disko.url = "github:nix-community/disko";
+    disko.inputs.nixpkgs.follows = "nixpkgs";
+
+
   };
 
-  outputs = { nixpkgs, home-manager, ... }: {
-    homeConfigurations = {
-      macos = home-manager.lib.homeManagerConfiguration {
-        pkgs = nixpkgs.legacyPackages.aarch64-darwin;
-        modules = [
-          ./modules
-          ./scripts
-          {
-            home.username = "pwnwriter";
-            home.stateVersion = "23.11";
-            home.homeDirectory = "/Users/pwnwriter";
-          }
-        ];
-      };
-
-      server = home-manager.lib.homeManagerConfiguration {
-        pkgs = nixpkgs.legacyPackages.aarch64-linux;
-        modules = [
-          ./modules
-          ./scripts
-          {
+  outputs = { nixpkgs, home-manager, disko, ... }: {
+    nixosConfigurations.wolf = nixpkgs.lib.nixosSystem {
+      system = "x86_64-linux";
+      modules = [
+        disko.nixosModules.disko
+        ./configuration.nix
+        ./disk-config.nix
+        home-manager.nixosModules.home-manager
+        {
+          home-manager.useGlobalPkgs = true;
+          home-manager.useUserPackages = true;
+          home-manager.users.wolf = {
             home.username = "wolf";
-            home.stateVersion = "23.11";
             home.homeDirectory = "/home/wolf";
-          }
-        ];
-      };
+            home.stateVersion = "23.11";
 
+            programs.home-manager.enable = true;
+            services.gpg-agent = {
+              enable = true;
+              pinentryFlavor = "tty";
+            };
+
+            imports = [
+              ./modules
+            ];
+          };
+        }
+      ];
     };
+
+    homeConfigurations.macos = home-manager.lib.homeManagerConfiguration {
+      pkgs = nixpkgs.legacyPackages.aarch64-darwin;
+      modules = [
+        ./modules
+        ./scripts
+        {
+          home.username = "pwnwriter";
+          home.stateVersion = "23.11";
+          home.homeDirectory = "/Users/pwnwriter";
+        }
+      ];
+    };
+
   };
 }
