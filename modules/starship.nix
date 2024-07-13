0df117aa2f7c3769e6cc22ef28d127d7201b44ff
pwnwriter <hey@pwnwriter.xyz>
2024-07-13 10:26:13 +0545

    feat(prompt): show git related infos as well

--- modules/starship.nix
+++ modules/starship.nix
@@ -42,11 +42,11 @@ in
       palette = "catppuccin_macchiato";
 
       format = lib.concatStrings [
-        "$nix_shell $directory$character"
+        "$directory$git_branch$character"
       ];
 
       right_format = lib.concatStrings [
-        ""
+        "$nix_shellc"
       ];
 
       character = if pkgs.stdenv.isDarwin then macos_prompt else linux_prompt;
@@ -62,6 +62,29 @@ in
         unknown_msg = "unknown";
         format = "[$symbol$state]($style)";
         symbol = "󱄅 ";
+      };
+
+      git_branch = {
+        symbol = "󰊢 ";
+        format = "on [$symbol$branch]($style) ";
+        truncation_length = 4;
+        truncation_symbol = "…/";
+        style = "bold green";
+      };
+      git_status = {
+        format = "[\\($all_status$ahead_behind\\)]($style) ";
+        style = "bold green";
+        conflicted = "🏳";
+        up_to_date = " ";
+        untracked = " ";
+        ahead = "⇡\${count}";
+        diverged = "⇕⇡\${ahead_count}⇣\${behind_count}";
+        behind = "⇣\${count}";
+        stashed = "󰏗 ";
+        modified = " ";
+        staged = "[++\\($count\\)](green)";
+        renamed = "󰖷 ";
+        deleted = " ";
       };
 
       palettes.catppuccin_macchiato = {
