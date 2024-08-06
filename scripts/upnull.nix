546dc6a5962788d89000d129a4e87fce9df44c10
pwnwriter <hey@pwnwriter.xyz>
2024-08-06 12:54:59 +0545

    feat(upnull): check host and set clip on buildtime

--- scripts/upnull.nix
+++ scripts/upnull.nix
@@ -1,12 +1,21 @@
 { pkgs, ... }:
 let
+  clip = if pkgs.stdenv.isDarwin then "pbcopy" else "wl-copy";
+
   upnull = pkgs.writeShellScriptBin "upnull" ''
     file=$1
     if [ -z "$file" ]; then
       echo "Usage: $0 <filename>"
       exit 1
     fi
-    ${pkgs.curl}/bin/curl -s -F "file=@$file" -H "Authorization: $(${pkgs.pass}/bin/pass personal/null)" https://null.pwnwriter.xyz
+
+    upload_url="https://null.pwnwriter.xyz"
+
+    response=$(${pkgs.curl}/bin/curl -s -F "file=@$file" -H "Authorization: $(${pkgs.pass}/bin/pass personal/null)" $upload_url)
+
+    echo "$response" | ${clip}
+
+    echo -e "$response\n is copied to clipboard."
   '';
 in
 {
