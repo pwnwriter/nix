74bc4098d79e69a39fb3aab20ed1115462f16bfd
pwnwriter <hi@pwnwriter.me>
2026-07-27 01:36:53 -0400

    feat(yazi): enable yazi to copy/paste stuffs on different sessions

--- modules/yazi.nix
+++ modules/yazi.nix
@@ -1,10 +1,15 @@
-_:
-
 {
   programs.yazi = {
     enable = true;
     enableZshIntegration = true;
   };
+
+  xdg.configFile."yazi/init.lua".text = ''
+    require("session"):setup {
+      sync_yanked = true,
+    }
+  '';
+
   catppuccin = {
     enable = true;
     accent = "maroon";
