905d1bfdb13ba03bb28788d2f4e1c2a153baee89
pwnwriter <hi@pwnwriter.me>
2026-09-10 22:40:22 -0400

    chore(darlene): add tailscale for darlene

--- machines/darlene/default.nix
+++ machines/darlene/default.nix
@@ -1,6 +1,7 @@
 { inputs }:
 (import ../../lib { inherit inputs; }).mkDarwin {
   modules = [
+    { services.tailscale.enable = true; }
     # Mac Mini specific settings
     {
       system.keyboard.enableKeyMapping = true;
